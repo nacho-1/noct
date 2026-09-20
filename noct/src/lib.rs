74d5271df4dbc50d8faff564bb467dd86bbc9609
nacho-1 <56204692+nacho-1@users.noreply.github.com>
2026-09-20 16:32:08 -0300

    refactor main function
    Modularize into subroutine.
    Modify shutdown handling.

--- noct/src/lib.rs
+++ noct/src/lib.rs
@@ -1,5 +1,20 @@
+use std::fs::File;
+
+use anyhow::Context as _;
+use aya::{Ebpf, programs::{CgroupAttachMode, CgroupSkb, CgroupSkbAttachType}};
+use aya_log::EbpfLogger;
+use clap::Parser;
+use log::{warn};
+use tokio::{io::{Interest, unix::AsyncFd}, signal::{self, unix::SignalKind}};
 use tracing_panic::panic_hook;
 use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
+/// Application options passed by arguments.
+#[derive(Debug, Parser)]
+pub struct Opts {
+    #[clap(short, long, default_value = "/sys/fs/cgroup")]
+    cgroup_path: std::path::PathBuf,
+}
 
 /// Initializes tracing.
 ///
@@ -23,4 +38,82 @@ pub fn init_tracing() {
         .init();
 
     std::panic::set_hook(Box::new(panic_hook));
+}
+
+/// Runs the application.
+///
+/// This function does all the work to initialize and run the application:
+///
+/// 1. Load the eBPF programs into kernel.
+pub async fn run(opts: Opts) -> anyhow::Result<()> {
+    // This will include the eBPF object file as raw bytes at compile-time
+    // and load it at runtime.
+    let mut ebpf = Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/noct"
+    )))?;
+
+    match EbpfLogger::init(&mut ebpf) {
+        Err(e) => {
+            // This can happen if all log statements are removed from the eBPF program.
+            warn!("failed to initialize eBPF logger: {e}");
+        }
+        Ok(logger) => {
+            let mut logger = AsyncFd::with_interest(logger, Interest::READABLE)?;
+            tokio::task::spawn(async move {
+                loop {
+                    let mut guard = logger.readable_mut().await.unwrap();
+                    guard.get_inner_mut().flush();
+                    guard.clear_ready();
+                }
+            });
+        }
+    }
+
+    let Opts { cgroup_path } = opts;
+    let cgroup = File::open(&cgroup_path).with_context(|| format!("{}", cgroup_path.display()))?;
+    let ingress_program: &mut CgroupSkb = ebpf.program_mut("ingress").unwrap().try_into()?;
+    ingress_program.load()?;
+    ingress_program.attach(
+        cgroup.try_clone()?,
+        CgroupSkbAttachType::Ingress,
+        CgroupAttachMode::default(),
+    )?;
+    let egress_program: &mut CgroupSkb = ebpf.program_mut("egress").unwrap().try_into()?;
+    egress_program.load()?;
+    egress_program.attach(
+        cgroup,
+        CgroupSkbAttachType::Egress,
+        CgroupAttachMode::default(),
+    )?;
+
+    println!("Waiting for Ctrl-C...");
+    shutdown_handler().await;
+    println!("Exiting...");
+
+    Ok(())
+}
+
+/// Handles shutdown of the application.
+///
+/// Will await for either a Ctrl-C (SIGINT)
+/// or a SIGTERM (usually sent by `docker stop`).
+async fn shutdown_handler() {
+    // Handle Ctrl-C (SIGINT)
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("Ctrl-C handler must be installed")
+    };
+
+    // Handle SIGTERM (sent by `docker stop`)
+    let mut terminate = signal::unix::signal(SignalKind::terminate())
+        .expect("SIGTERM handler must be installed");
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate.recv() => {},
+    }
+
+    tracing::info!("Shutdown signal received");
 }
--- noct/src/main.rs
+++ noct/src/main.rs
@@ -1,76 +1,23 @@
-use std::fs::File;
+use std::process::ExitCode;
 
-use anyhow::Context as _;
-use aya::{
-    Ebpf,
-    programs::{CgroupSkb, CgroupSkbAttachType, links::CgroupAttachMode},
-};
-use aya_log::EbpfLogger;
 use clap::Parser;
-#[rustfmt::skip]
-use log::warn;
-use tokio::{
-    io::{Interest, unix::AsyncFd},
-    signal, task,
-};
-
-#[derive(Debug, Parser)]
-struct Opt {
-    #[clap(short, long, default_value = "/sys/fs/cgroup")]
-    cgroup_path: std::path::PathBuf,
-}
+use noct::Opts;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let opt = Opt::parse();
+async fn main() -> ExitCode {
+    let opts = Opts::parse();
 
     noct::init_tracing();
 
-    // This will include the eBPF object file as raw bytes at compile-time
-    // and load it at runtime.
-    let mut ebpf = Ebpf::load(aya::include_bytes_aligned!(concat!(
-        env!("OUT_DIR"),
-        "/noct"
-    )))?;
-
-    match EbpfLogger::init(&mut ebpf) {
+    match noct::run(opts).await {
+        Ok(_) => ExitCode::SUCCESS,
         Err(e) => {
-            // This can happen if all log statements are removed from the eBPF program.
-            warn!("failed to initialize eBPF logger: {e}");
-        }
-        Ok(logger) => {
-            let mut logger = AsyncFd::with_interest(logger, Interest::READABLE)?;
-            task::spawn(async move {
-                loop {
-                    let mut guard = logger.readable_mut().await.unwrap();
-                    guard.get_inner_mut().flush();
-                    guard.clear_ready();
-                }
-            });
+            tracing::error!(
+                error.msg = %e,
+                error.error_chain = ?e,
+                "Shutting down due to error"
+            );
+            ExitCode::FAILURE
         }
     }
-
-    let Opt { cgroup_path } = opt;
-    let cgroup = File::open(&cgroup_path).with_context(|| format!("{}", cgroup_path.display()))?;
-    let ingress_program: &mut CgroupSkb = ebpf.program_mut("ingress").unwrap().try_into()?;
-    ingress_program.load()?;
-    ingress_program.attach(
-        cgroup.try_clone()?,
-        CgroupSkbAttachType::Ingress,
-        CgroupAttachMode::default(),
-    )?;
-    let egress_program: &mut CgroupSkb = ebpf.program_mut("egress").unwrap().try_into()?;
-    egress_program.load()?;
-    egress_program.attach(
-        cgroup,
-        CgroupSkbAttachType::Egress,
-        CgroupAttachMode::default(),
-    )?;
-
-    let ctrl_c = signal::ctrl_c();
-    println!("Waiting for Ctrl-C...");
-    ctrl_c.await?;
-    println!("Exiting...");
-
-    Ok(())
 }
